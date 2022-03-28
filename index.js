165d21887e2c6f7332ab6fdf4d4270b4ac9b3d2d
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-03-28 13:52:46 +0530

    database connected

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+const mongoString = process.env.DATABASE_URL
+
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
+
+
+//mongodb+srv://sarvesh:<password>@cluster0.anzgr.mongodb.net/test
